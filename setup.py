46ee05eedb911fe3a278ff6f31ebb852ebe13380
Marco Bakera <marco@bakera.de>
2017-04-19 20:51:54 +0200

    enhancing setup.py version.

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-"""Kata 13 - fizzbuzz als installierbares Programm.
-
-Das Programm kann mit folgenden Befehlen installiert oder erstellt
-werden:
-
-Release in Unterordner erstellen: 
-$ python3 setup.py sdist
-
-Installation:
-$ python3 setup.py install
-
-Lokale Installation:
-$ python3 setup.py install --user
-
-"""
-
 from setuptools import setup, find_packages
 
 setup(
