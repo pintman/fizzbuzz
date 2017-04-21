1c3b62ac37cd092345710023337a924a91c2576c
Marco Bakera <marco@bakera.de>
2017-04-21 11:44:40 +0200

    docker example uses environment vars for running.

--- fizzbuzz_docker.py
+++ fizzbuzz_docker.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 def fizzbuzz(n):
     if n % 3 == 0 and n % 5 == 0:
@@ -10,6 +11,18 @@ def fizzbuzz(n):
     else:
         return n
 
-n = int(sys.argv[1])    
+# determine parameter for fizzbuzz: environment variable, commandline,
+# or default value.
+if "FIZZBUZZ" in os.environ:
+    print("[using environment variable as parameter]")
+    n = os.environ["FIZZBUZZ"]
+    n = int(n)    
+elif len(sys.argv) > 1:
+    print("[using command line parameter]")
+    n = int(sys.argv[1])
+else:
+    print("[using default value as parameter]")
+    n = 20   
+
 for i in range(n):
     print(fizzbuzz(i))
